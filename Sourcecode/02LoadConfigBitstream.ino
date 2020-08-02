b2ca64160c387ab47d97cd1c7850dec858d72991
fuqin-feilong <15682530085@163.com>
2020-08-02 12:30:40 +0800

    1

---
+++ Sourcecode/02LoadConfigBitstream.ino
@@ -0,0 +1,93 @@
+/*
+ * 02LoadConfigBitstream
+ * 
+ * reading the board_config.ini file.
+ * getting the value of overlay_on_boot,
+ * and ESP32 loading it
+ * 
+ * If you want more details, read the README.md
+ * 
+ * The MIT License (MIT)
+ * Copyright (C) 2019  Seeed Technology Co.,Ltd.
+ */
+
+// include the library:
+#include <spartan-edge-esp32-boot.h>
+#include "ESP32IniFile.h"
+#include "sea_esp32_qspi.h"
+
+  
+// initialize the spartan_edge_esp32_boot library
+spartan_edge_esp32_boot esp32Cla;
+
+const size_t bufferLen = 80;
+char buffer[bufferLen];
+char buffer1[bufferLen];
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialization 
+  esp32Cla.begin();
+
+  // check the .ini file exist or not
+  const char *filename = "/board_config.ini";
+  IniFile ini(filename);
+  if (!ini.open()) {
+    Serial.print("Ini file ");
+    Serial.print(filename);
+    Serial.println(" does not exist");
+    return;
+  }
+  Serial.println("Ini file exists");
+
+  // check the .ini file valid or not
+  if (!ini.validate(buffer, bufferLen)) {
+    Serial.print("ini file ");
+    Serial.print(ini.getFilename());
+    Serial.print(" not valid: ");
+    return;
+  }
+
+  // Fetch a value from a key which is present
+  if (ini.getValue("Overlay_List_Info", "Overlay_Dir", buffer, bufferLen)) {
+    Serial.print("section 'Overlay_List_Info' has an entry 'Overlay_Dir' with value ");
+    Serial.println(buffer);
+  }
+  else {
+    Serial.print("Could not read 'Overlay_List_Info' from section 'Overlay_Dir', error was ");
+  }
+
+  // Fetch a value from a key which is present
+  if (ini.getValue("Board_Setup", "overlay_on_boot", buffer1, bufferLen)) {
+    Serial.print("section 'Board_Setup' has an entry 'overlay_on_boot' with value ");
+    Serial.println(buffer1);
+  }
+  else {
+    Serial.print("Could not read 'Board_Setup' from section 'overlay_on_boot', error was ");
+  }
+
+  // Splicing characters
+  strcat(buffer,buffer1);
+  
+  // XFPGA pin Initialize
+  esp32Cla.xfpgaGPIOInit();
+
+  // loading the bitstream
+  esp32Cla.xlibsSstream(buffer);
+
+  uint8_t data1[12]={97,97,97,97,  97,97,97,97, 97,97,97,97};
+  uint8_t data2[16] = {0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0};
+  Serial.begin(115200);
+  SeaTrans.begin();
+  SeaTrans.write(0, data1, 12);
+  SeaTrans.read(16, data2,16);
+  int mm=0;
+  for(mm=0;mm<16;mm++)
+  {
+  Serial.printf("%x \r",data2[mm]);
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // nothink to do
+}
